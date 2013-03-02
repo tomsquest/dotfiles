4a596aeb20b4961b0b6f77935a99d91a8bb79d6b
Thomas Queste <tqueste@gmail.com>
2013-03-02 11:04:57 +0100

    Test existence of beep/notify-send

--- zsh/functions/alert.zsh
+++ zsh/functions/alert.zsh
@@ -22,11 +22,19 @@ function alert() {
 }
 
 function notify_success() {
-  /usr/bin/notify-send -i "terminal" "Success" "$1"
-  /usr/bin/beep -l 100 -f 500 -n -l 50 -f 1000
+  if [ -f /usr/bin/notify-send ]; then
+    /usr/bin/notify-send -i "terminal" "Success" "$1"
+  fi
+  if [ -f /usr/bin/beep ]; then
+    /usr/bin/beep -l 100 -f 500 -n -l 50 -f 1000
+  fi
 }
 
 function notify_failure() {
-  /usr/bin/notify-send -i "error" "Error: $2" "$1"
-  /usr/bin/beep -l 100 -f 10 -r 2
+  if [ -f /usr/bin/notify-send ]; then
+    /usr/bin/notify-send -i "error" "Error: $2" "$1"
+  fi
+  if [ -f /usr/bin/beep ]; then
+    /usr/bin/beep -l 100 -f 100 -r 2
+  fi
 }
