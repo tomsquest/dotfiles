fc998b9cc757aad7f1d4355b8554a74f1aa65d3a
Thomas Queste <tqueste@gmail.com>
2014-04-30 14:58:27 +0200

    Remove Rvm instruction to force rvm reload, useless now

--- zsh/rvm.zsh
+++ zsh/rvm.zsh
@@ -2,8 +2,5 @@
 # http://beginrescueend.com/integration/zsh/
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-# Required when opening a new tab, RVM should be "reloaded"
-__rvm_project_rvmrc
-
 # Add RVM to PATH for scripting
 export PATH=$PATH:$HOME/.rvm/bin
