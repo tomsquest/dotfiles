f644e6a6aecdbebe97d059e92184d1fc0b2943fb
Thomas Queste <tqueste@gmail.com>
2014-10-17 14:24:56 +0200

    Fix typo

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -91,7 +91,7 @@ bindkey '\C-x\C-e' edit-command-line
 # Example: zmv '(**/)file.xml' '$1anotherName.xml'
 autoload zmv
 
-# Load ruma Z: quickly jump to recent directory with the z command
+# Load rupa Z: quickly jump to recent directory with the z command
 # Example: z foo
 # See: https://github.com/rupa/z
 source ~/.zsh/rupa-z/z.sh
