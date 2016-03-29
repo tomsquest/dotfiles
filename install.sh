ffac34ac2d45ab0fb77aece9fc19d32835d986f5
Thomas Queste <tom@tomsquest.com>
2016-03-29 08:42:06 +0200

    Add Lubuntu openbox.xml

--- install.sh
+++ install.sh
@@ -13,18 +13,19 @@ function link_if_missing() {
   fi
 }
 
-link_if_missing $PWD/bash       $HOME/.bash
-link_if_missing $PWD/bin        $HOME/bin
-link_if_missing $PWD/vim        $HOME/.vim
-link_if_missing $PWD/zsh        $HOME/.zsh
-link_if_missing $PWD/bashrc     $HOME/.bashrc
-link_if_missing $PWD/ackrc      $HOME/.ackrc
-link_if_missing $PWD/gitconfig  $HOME/.gitconfig
-link_if_missing $PWD/gitignore  $HOME/.gitignore
-link_if_missing $PWD/imwheelrc  $HOME/.imwheelrc
-link_if_missing $PWD/irbrc      $HOME/.irbrc
-link_if_missing $PWD/plsqlrc    $HOME/.plsqlrc
-link_if_missing $PWD/rvmrc      $HOME/.rvmrc
-link_if_missing $PWD/tmux.conf  $HOME/.tmux.conf
-link_if_missing $PWD/vimrc      $HOME/.vimrc
-link_if_missing $PWD/zshrc      $HOME/.zshrc
+link_if_missing $PWD/bash                   $HOME/.bash
+link_if_missing $PWD/bin                    $HOME/bin
+link_if_missing $PWD/vim                    $HOME/.vim
+link_if_missing $PWD/zsh                    $HOME/.zsh
+link_if_missing $PWD/bashrc                 $HOME/.bashrc
+link_if_missing $PWD/ackrc                  $HOME/.ackrc
+link_if_missing $PWD/gitconfig              $HOME/.gitconfig
+link_if_missing $PWD/gitignore              $HOME/.gitignore
+link_if_missing $PWD/imwheelrc              $HOME/.imwheelrc
+link_if_missing $PWD/irbrc                  $HOME/.irbrc
+link_if_missing $PWD/plsqlrc                $HOME/.plsqlrc
+link_if_missing $PWD/rvmrc                  $HOME/.rvmrc
+link_if_missing $PWD/tmux.conf              $HOME/.tmux.conf
+link_if_missing $PWD/vimrc                  $HOME/.vimrc
+link_if_missing $PWD/zshrc                  $HOME/.zshrc
+link_if_missing $PWD/openbox/openbox.xml    $HOME/.config/openbox/lubuntu-rc.xml
