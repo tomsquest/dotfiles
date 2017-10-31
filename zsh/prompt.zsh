f516c6de4fdcb644955b47c13935febbca3afe84
Thomas Queste <tom@tomsquest.com>
2017-10-31 07:51:55 +0100

    Clean prompt vars

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,3 @@
-autoload -U promptinit
-promptinit
-
 # Enable parameter expansion, command substitution and arithmetic expansion in prompts
 setopt prompt_subst
 
@@ -10,40 +7,32 @@ colors;
 
 # Create a separation bar after the path
 # see: http://aperiodic.net/phil/prompt/
-function precmd {
+function compute-separation-bar {
   # Compute the lengths of the strings
   local exitcodesize=${#${(%):-%(?..%? )}}
   local promptsize=${#${(%):-hh-mm-ss-}}
   local pwdsize=${#${(%):-$PWD}}
-
-  # Global width
-  local TERMWIDTH
-  (( TERMWIDTH = ${COLUMNS} - 1 ))
+  local termwidth=$(($COLUMNS - 1))
 
   # The horizontal bar
-  PR_FILLBAR=""
+  barsize=""
   # The path, truncated if too long
-  PR_PWDLEN=""
+  pathsize=""
 
   # Compute the path length and the horizontal bar
-  if [[ "$promptsize + $pwdsize + $exitcodesize" -gt $TERMWIDTH ]]; then
-    ((PR_PWDLEN=$TERMWIDTH - $promptsize - $exitcodesize))
+  if [[ "$promptsize + $pwdsize + $exitcodesize" -gt $termwidth ]]; then
+    ((pathsize=$termwidth - $promptsize - $exitcodesize))
   else
-    PR_FILLBAR="\${(l.(($TERMWIDTH - ($promptsize + $pwdsize + $exitcodesize)))..—.)}"
+    barsize="\${(l.(($termwidth - $promptsize - $pwdsize - $exitcodesize))..—.)}"
   fi
-
 }
-
-# Change user/host color if root
-if [ $UID -eq 0 ]; then usercolor="red"; else usercolor="green"; fi
+add-zsh-hook precmd compute-separation-bar
 
 # Finally, set the prompt
-#local user_and_host="%{$fg_bold[$usercolor]%}%n@%m%{$reset_color%}"
-local user_and_host=""
 local the_date="%{$fg_bold[green]%}%D{%H:%M:%S}%{$reset_color%}"
 local last_command_status="%(?..%{$fg_bold[red]%}%? %{$reset_color%})"
 local start_of_input="%{$fg_bold[white]%}$%{$reset_color%}"
-PROMPT='$the_date %{$fg_bold[blue]%}%$PR_PWDLEN<...<$PWD%<<%{$reset_color%} $last_command_status%{$fg_bold[white]%}${(e)PR_FILLBAR}%{$reset_color%}
+PROMPT='$the_date %{$fg_bold[blue]%}%$pathsize<...<$PWD%<<%{$reset_color%} $last_command_status%{$fg_bold[white]%}${(e)barsize}%{$reset_color%}
 $start_of_input '
 
 # Git prompt, displayed at right (RPROMPT)
