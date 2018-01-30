94123002a7b726d6c5d453113a2758dc14998f17
Thomas Queste <tom@tomsquest.com>
2018-01-30 15:52:29 +0100

    feat: install latest release of FZF

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 
 set -euo pipefail
 
-# Fetch the code from the other Git repos like zsh-git-prompt, Vim vundle...
-echo "Fetching submodules..."
-git submodule update --remote --init
+function install-submodules {
+    echo "Fetching submodules..."
+    git submodule update --remote --init
+}
 
-# Create the symlinks in $HOME
-function link_if_missing() {
+function mkdir-paths {
+    echo "Creating additional PATH directories..."
+    mkdir -p "$HOME/.local/bin"
+}
+
+function create-link {
   SRC=$1
   DEST=$2
 
@@ -19,31 +24,49 @@ function link_if_missing() {
   fi
 }
 
-echo "Creating additional PATH directories..."
-mkdir -p "$HOME/.local/bin"
+function install-fzf {
+    echo "Installing FZF..."
+    readonly URL=$(curl -s https://api.github.com/repos/junegunn/fzf-bin/releases/latest | grep browser_download_url | grep linux_amd64 | cut -d '"' -f 4)
+    curl -sL "$URL" | tar xz fzf -C /tmp \
+        && mv fzf ~/.local/bin/
+}
 
-echo "Linking files..."
-link_if_missing "$PWD/bin"                    "$HOME/bin"
-link_if_missing "$PWD/bash"                   "$HOME/.bash"
-link_if_missing "$PWD/zsh"                    "$HOME/.zsh"
-link_if_missing "$PWD/vim"                    "$HOME/.vim"
-link_if_missing "$PWD/bashrc"                 "$HOME/.bashrc"
-link_if_missing "$PWD/ackrc"                  "$HOME/.ackrc"
-link_if_missing "$PWD/gitconfig"              "$HOME/.gitconfig"
-link_if_missing "$PWD/gitignore"              "$HOME/.gitignore"
-link_if_missing "$PWD/imwheelrc"              "$HOME/.imwheelrc"
-link_if_missing "$PWD/irbrc"                  "$HOME/.irbrc"
-link_if_missing "$PWD/plsqlrc"                "$HOME/.plsqlrc"
-link_if_missing "$PWD/rvmrc"                  "$HOME/.rvmrc"
-link_if_missing "$PWD/tmux.conf"              "$HOME/.tmux.conf"
-link_if_missing "$PWD/vimrc"                  "$HOME/.vimrc"
-link_if_missing "$PWD/zshrc"                  "$HOME/.zshrc"
-link_if_missing "$PWD/terminator.conf"        "$HOME/.config/terminator/config"
-link_if_missing "$PWD/openbox.xml"            "$HOME/.config/openbox/lubuntu-rc.xml"
-for file in $PWD/desktop-shortcuts/*
-do
-  link_if_missing "$file" "$HOME/.local/share/applications/$(basename "$file")"
-done
+function link-files {
+    echo "Linking files..."
+    create-link "$PWD/bin"                    "$HOME/bin"
+    create-link "$PWD/bash"                   "$HOME/.bash"
+    create-link "$PWD/zsh"                    "$HOME/.zsh"
+    create-link "$PWD/vim"                    "$HOME/.vim"
+    create-link "$PWD/bashrc"                 "$HOME/.bashrc"
+    create-link "$PWD/ackrc"                  "$HOME/.ackrc"
+    create-link "$PWD/gitconfig"              "$HOME/.gitconfig"
+    create-link "$PWD/gitignore"              "$HOME/.gitignore"
+    create-link "$PWD/imwheelrc"              "$HOME/.imwheelrc"
+    create-link "$PWD/irbrc"                  "$HOME/.irbrc"
+    create-link "$PWD/plsqlrc"                "$HOME/.plsqlrc"
+    create-link "$PWD/rvmrc"                  "$HOME/.rvmrc"
+    create-link "$PWD/tmux.conf"              "$HOME/.tmux.conf"
+    create-link "$PWD/vimrc"                  "$HOME/.vimrc"
+    create-link "$PWD/zshrc"                  "$HOME/.zshrc"
+    create-link "$PWD/terminator.conf"        "$HOME/.config/terminator/config"
+    create-link "$PWD/openbox.xml"            "$HOME/.config/openbox/lubuntu-rc.xml"
+    for file in $PWD/desktop-shortcuts/*
+    do
+      create-link "$file" "$HOME/.local/share/applications/$(basename "$file")"
+    done
+}
 
-echo "Installing Vim plugins..."
-vim +PluginInstall +qall
+function install-vim-plugins {
+    echo "Installing Vim plugins..."
+    vim +PluginInstall +qall
+}
+
+function main {
+    mkdir-paths
+    link-files
+    install-fzf
+    install-submodules
+    install-vim-plugins
+}
+
+main
