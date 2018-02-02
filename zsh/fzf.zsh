99c49577d307a576f5f444b5e1c90a50c2ce25aa
Thomas Queste <tom@tomsquest.com>
2018-02-02 11:10:41 +0100

    feat: use ripgrep when using completion (cd **, vim **)

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -4,3 +4,14 @@ export FZF_DEFAULT_COMMAND='rg --files --hidden --follow 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 source $HOME/.local/fzf/completion.zsh
+
+#
+# Overrides completion, eg. `cd **`, `vim **`
+#
+_fzf_compgen_path() {
+  rg --files --hidden --follow -g "!{.git,node_modules}/*" "$1"
+}
+
+_fzf_compgen_dir() {
+  rg --files --hidden --null -g "!{.git,node_modules}/*" "$1" | xargs -0 dirname | sort -u
+}
