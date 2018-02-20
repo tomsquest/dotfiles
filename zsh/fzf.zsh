8b85f5fc4a186b94261fd57b40035ee1686b6eb6
Thomas Queste <tom@tomsquest.com>
2018-02-20 15:59:57 +0100

    feat(fzf): don't show error for unreadable file
    close #14

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,8 +1,8 @@
 export FZF_DEFAULT_OPTS='--height=50% --reverse --multi --preview="[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (highlight -O ansi -l {} || coderay {} || rougify {} || cat {}) 2> /dev/null | head -500"'
 
-export FZF_DEFAULT_COMMAND='rg --files --hidden'
+export FZF_DEFAULT_COMMAND='rg --files --hidden 2>/dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="rg --files --hidden --null "$1" | xargs -0 dirname | sort -u"
+export FZF_ALT_C_COMMAND="rg --files --hidden --null "$1" 2>/dev/null | xargs -0 dirname | sort -u"
 
 source $HOME/.local/fzf/completion.zsh
 source $HOME/.local/fzf/key-bindings.zsh
@@ -11,11 +11,11 @@ source $HOME/.local/fzf/key-bindings.zsh
 # Overrides completion, eg. `cd **`, `vim **`
 #
 _fzf_compgen_path() {
-  rg --files --hidden "$1"
+  rg --files --hidden "$1" 2>/dev/null
 }
 
 _fzf_compgen_dir() {
-  rg --files --hidden --null "$1" | xargs -0 dirname | sort -u
+  rg --files --hidden --null "$1" 2>/dev/null | xargs -0 dirname | sort -u
 }
 
 # Open the selected file with keybindings
