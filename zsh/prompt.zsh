86e4ec282ff4c3eb5f652bbf194b151db3839a11
Thomas Queste <tom@tomsquest.com>
2018-06-21 10:36:12 +0200

    chore: replace zsh-git-prompt with native zsh vcs_info

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,30 +7,46 @@ setopt prompt_subst
 autoload -U colors
 colors
 
-function build_prompt {
-    # Exit code needs to be computed first
-    local -r exit_code="${(%):-%(?..%? )}"
-    local -r time="$(date +"%T")"
+# Configure ZSH vcs module
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' formats "%b %u%c"
+zstyle ':vcs_info:*' actionformats "%b %u%c %a"
+zstyle ':vcs_info:*' check-for-changes true
 
-    # Build separator bar
-    local -r separator="―"
-    local -r assembled_prompt="$time $PWD $exit_code"
+function build_prompt {
+    # Exit code of last command
+    # - Needs to be computed first
+    # - if not zero, add a space after it
+    local -r exit_code="${(%):-%(?..%? )}"
+
+    # Prompt elements
+    vcs_info
+    local -r gitinfo="${vcs_info_msg_0_%% }" # Trim possible trailing space
+    local -r time="$(date +"%T")"
+    local -r assembled_prompt="$time $PWD $exit_code $gitinfo"
     local -r assembled_prompt_length=${#assembled_prompt}
-    local -r bar_length=$((COLUMNS - ($assembled_prompt_length % COLUMNS)))
+
+    # Separator bar
+    local -r separator="―"
+    local -r bar_length=$((COLUMNS - assembled_prompt_length % COLUMNS))
     local -r bar="\${(l.(($bar_length))..$separator.)}"
 
     # Build prompt
-    local -r colored_exit="%{$fg_bold[red]%}$exit_code%{$reset_color%}"
-    local -r colored_time="%{$fg_bold[green]%}$time%{$reset_color%}"
-    local -r colored_path="%{$fg_bold[blue]%}$PWD%{$reset_color%}"
-    local -r colored_bar="%{$fg_bold[white]%}$bar%{$reset_color%}"
-    local -r start_of_input="%{$fg_bold[white]%}$%{$reset_color%}"
-    PROMPT="$colored_time $colored_path $colored_exit$colored_bar
+    local -r colored_exit="%B%F{red}$exit_code%f%b"
+    local -r colored_time="%B%F{green}$time%f%b"
+    local -r colored_path="%B%F{blue}$PWD%f%b"
+    local -r colored_bar="%B%F{white}$bar%f%b"
+    # Color GIT info depending of status: unstaged/staged
+    if [[ $gitinfo = *[\ ]* ]]; then
+      local -r colored_gitinfo="%B%F{yellow}$gitinfo%f%b"
+    else
+      local -r colored_gitinfo="%B%F{green}$gitinfo%f%b"
+    fi
+
+    local -r start_of_input="%B%F{white}$%f%b"
+    PROMPT="$colored_time $colored_path $colored_gitinfo $colored_exit$colored_bar
 $start_of_input "
 }
 
 add-zsh-hook precmd build_prompt
-
-# Git prompt
-# See: https://github.com/olivierverdier/zsh-git-prompt
-RPROMPT='$(git_super_status)'
--- zshrc
+++ zshrc
@@ -10,7 +10,6 @@ if ! zgen saved; then
   zgen oh-my-zsh plugins/httpie
   zgen oh-my-zsh plugins/command-not-found
   zgen load zsh-users/zsh-completions src
-  zgen load olivierverdier/zsh-git-prompt
   zgen load chrissicool/zsh-256color
   zgen load johnhamelink/rvm-zsh
   zgen load johnhamelink/env-zsh
