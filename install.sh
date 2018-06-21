fe78b97833263fcf0099e0a4a00315b49fa93b05
Thomas Queste <tom@tomsquest.com>
2018-06-21 11:34:59 +0200

    chore: delete ackrc

--- install.sh
+++ install.sh
@@ -88,7 +88,6 @@ function link-files {
     create-link "$PWD/zsh"                    "$HOME/.zsh"
     create-link "$PWD/vim"                    "$HOME/.vim"
     create-link "$PWD/bashrc"                 "$HOME/.bashrc"
-    create-link "$PWD/ackrc"                  "$HOME/.ackrc"
     create-link "$PWD/gitconfig"              "$HOME/.gitconfig"
     create-link "$PWD/gitignore"              "$HOME/.gitignore"
     create-link "$PWD/imwheelrc"              "$HOME/.imwheelrc"
