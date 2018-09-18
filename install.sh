be9e00bcbe51bf40808dd360df0ffc6397b91c4b
Thomas Queste <tom@tomsquest.com>
2018-09-18 14:43:42 +0200

    feat: add ignored dirs for Albert launcher
    Close #17

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ function link-files {
     create-link "$PWD/bin"                    "$HOME/bin"
     create-link "$PWD/zsh"                    "$HOME/.zsh"
     create-link "$PWD/vim"                    "$HOME/.vim"
+    create-link "$PWD/albertignore"           "$HOME/.albertignore"
     create-link "$PWD/bashrc"                 "$HOME/.bashrc"
     create-link "$PWD/gitconfig"              "$HOME/.gitconfig"
     create-link "$PWD/gitignore"              "$HOME/.gitignore"
