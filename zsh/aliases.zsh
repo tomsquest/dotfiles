cb89b88c60c64142e66ee66dc8fe4d66fb3f4101
Thomas Queste <tom@tomsquest.com>
2019-07-31 11:29:58 +0200

    fix: missing `run` in NPM aliases
    Signed-off-by: Thomas Queste <tom@tomsquest.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -78,8 +78,8 @@ alias  msb='printAndRun mvn spring-boot:run'
 
 # NPM
 alias  ns='printAndRun npm start'
-alias nsd='printAndRun npm start:dev'
-alias nsw='printAndRun npm start:hmr'
+alias nsd='printAndRun npm run start:dev'
+alias nsw='printAndRun npm run start:hmr'
 alias  nt='printAndRun npm test'
 alias ntw='printAndRun npm run test:watch'
 
