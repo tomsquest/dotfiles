df2b0983737e746e6f385b429db21329cbd493bd
Thomas Queste <tom@tomsquest.com>
2023-10-22 17:16:23 +0200

    fix(fzf): use current dir always ($1 was not working)

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,7 +2,7 @@ export FZF_DEFAULT_OPTS='--height=50% --layout=reverse --info=inline --multi --p
 
 export FZF_DEFAULT_COMMAND='rg --files --hidden 2>/dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="rg --files --hidden --null \"$1\" 2>/dev/null | xargs -0 dirname | sort -u"
+export FZF_ALT_C_COMMAND="rg --files --hidden --null . 2>/dev/null | xargs -0 dirname | sort -u"
 
 # Disable preview, useless for History completion
 export FZF_CTRL_R_OPTS="--no-preview"
