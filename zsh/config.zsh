a25713b0f1af61f6d97b688a969629e04e126fd4
Thomas Queste <tom@tomsquest.com>
2024-11-14 06:42:28 +0100

    feat: use bat when using man for colors

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -60,23 +60,12 @@ export TMPDIR="/tmp"
 export EDITOR="/usr/bin/vim"
 export VISUAL="/usr/bin/vim"
 eval "$(dircolors -b ~/.dircolors)"
+# Less options
 export LESS="--ignore-case --RAW-CONTROL-CHARS --LONG-PROMPT --hilite-unread --tabs=2"
+# Enable using less on archive (eg. `less foo.zip`)
 eval $(lesspipe)
-# Colors for less binary.
-# Src: https://unix.stackexchange.com/questions/119/colors-in-man-pages/147#147
-export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # green
-export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # cyan
-export LESS_TERMCAP_me=$(tput sgr0)
-export LESS_TERMCAP_so=$(tput bold; tput setaf 3; tput setab 4) # yellow on blue
-export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # white
-export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
-export LESS_TERMCAP_mr=$(tput rev)
-export LESS_TERMCAP_mh=$(tput dim)
-export LESS_TERMCAP_ZN=$(tput ssubm)
-export LESS_TERMCAP_ZV=$(tput rsubm)
-export LESS_TERMCAP_ZO=$(tput ssupm)
-export LESS_TERMCAP_ZW=$(tput rsupm)
+# Use bat to color manpage
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 # Print dates in ISO format (in `ls -l` for example)
 export TIME_STYLE="long-iso"
 # Ripgrep config
